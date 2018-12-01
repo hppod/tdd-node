6645d55bd2b2956b2f88aacc380883663b7a5fcc
hppod <hppodgornik@gmail.com>
2018-12-01 18:22:23 -0200

    escrevendo metodos para testar as outras funcções da calculadora

--- src/test/calculadora-test.js
+++ src/test/calculadora-test.js
@@ -3,11 +3,21 @@ const calculadora = require('../demo-1/calculadora');
 
 const assert = chai.assert;
 
-describe('TDD de Operações da Calculadora', ()=> {
+describe('TDD de Operações da Calculadora', () => {
 
     it('Teste: Deve somar 2 números', () => {
-        assert.equal(calculadora.adicionar(5,5),10);
+        assert.equal(calculadora.adicionar(5, 5), 10);
     });
 
-    
-})
+    it('Teste: Deve subtrair 2 números', () => {
+        assert.equal(calculadora.subtrair(10, 5), 5);
+    });
+
+    it('Teste: Deve multiplicar 2 números', () => {
+        assert.equal(calculadora.multiplicar(10, 5), 50);
+    });
+
+    it('Teste: Deve dividir 2 números', () => {
+        assert.equal(calculadora.dividir(18, 2), 9);
+    });
+});
