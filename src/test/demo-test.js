94a817829d824a9919842314ab4eb9f01d7133a6
hppod <hppodgornik@gmail.com>
2018-12-01 18:31:24 -0200

    corrigindo

--- src/test/demo-test.js
+++ src/test/demo-test.js
@@ -1,7 +1,7 @@
 const assert = require('chai').assert;
 const demo = require('../demo-2/demo');
 
-describe('Diferentews tipos de testes com Mocha & Chai: ', () => {
+describe('Diferentes tipos de testes com Mocha & Chai: ', () => {
     it('Teste: Deve retornar a frase: "Mocha & Chai são legais!"', () => {
         assert.equal(demo(), 'Mocha & Chai são legais!');
     });
