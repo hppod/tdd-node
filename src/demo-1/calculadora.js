996f9568f10c32c371473d7bf4d7ee012fcebfd3
hppod <hppodgornik@gmail.com>
2018-12-01 18:16:49 -0200

    escrendendo funções da calculadora

---
+++ src/demo-1/calculadora.js
@@ -0,0 +1,14 @@
+module.exports = {
+
+    // Função para somar
+    adicionar: (num1, num2) => num1 + num2,
+
+    // Função para subtrair
+    subtrair: (num1, num2) => num1 - num2,
+
+    // Função para multiplicar
+    multiplicar: (num1, num2) => num1 * num2,
+
+    // Função para dividir
+    dividir: (num1, num2) => num1 / num2,
+};
